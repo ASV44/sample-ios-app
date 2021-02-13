3424c46e14e39bef1cf695df1d811b692fb1a004
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2021-02-13 13:41:31 +0200

    Change project structure and clean up project

--- SampleApp/Presentation/MainScreen/ViewController.swift
+++ SampleApp/Presentation/MainScreen/ViewController.swift
@@ -30,21 +30,4 @@ final class ViewController: UIViewController {
 
         navigationController?.pushViewController(chatVC, animated: true)
     }
-
-    private func instantiateViewController<T: UIViewController>(storyboardName: String,
-                                                                identifier: String,
-                                                                type: T.Type) -> T {
-        let storyboard: UIStoryboard = UIStoryboard(
-            name: storyboardName,
-            bundle: Bundle(for: type)
-        )
-
-        guard let viewController = storyboard.instantiateViewController(withIdentifier: identifier) as? T else {
-            fatalError("Could not instantiate \(identifier) from storyboard file.")
-        }
-
-        viewController.modalPresentationStyle = .fullScreen
-
-        return viewController
-    }
 }
